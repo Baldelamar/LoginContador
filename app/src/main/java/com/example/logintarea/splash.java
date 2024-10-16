5ebc4408513b3ee92d6a22fc2bd286863e114a7f
Jared Baldemar <baldelamar917@hotmail.com>
2024-10-16 07:43:55 -0600

    Agregado el texto del contador restante

--- app/src/main/java/com/example/logintarea/splash.java
+++ app/src/main/java/com/example/logintarea/splash.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 
 import android.content.Intent;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,21 +14,31 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class splash extends AppCompatActivity {
 
+    private TextView countdownText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
-        new CountDownTimer(5000, 1000){
+
+        // ENCUENTRO EL TEXTVIEW
+        countdownText = findViewById(R.id.tv_countdown);
+
+        // CONFIGURO EL CONTADOR EN 5 SEGUNDOS
+        new CountDownTimer(5000, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
-
+                // ACTUALIZAR EL TEXT CADA 1 SEGUNDO
+                countdownText.setText(String.valueOf(millisUntilFinished / 1000));
             }
+
             @Override
             public void onFinish() {
+                // Cuando TERMINE EL CONTADOR INICIE EL MAINACTIVITY
                 startActivity(new Intent(splash.this, MainActivity.class));
+                finish();
             }
         }.start();
-
     }
 }
